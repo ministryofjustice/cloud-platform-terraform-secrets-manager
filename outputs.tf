acd3184f5c42b7c28fcbaaed4be1b26dd6b15ce0
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-05-10 20:18:26 +0100

    Output secrets store name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "irsa_policy_arn" {
   value       = aws_iam_policy.irsa_policy.arn
   description = "The policy arn that grants access to the secret."
 }
+output "secret_store_name" {
+  value = local.secret_store_name
+  description = "value of the secret store name"
+}
