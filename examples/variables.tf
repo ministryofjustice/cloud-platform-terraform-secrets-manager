e724ac3dac6e923da765bdeeac7d935db5c5a25b
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2023-05-11 11:00:43 +0100

    fixing variables

--- examples/variables.tf
+++ examples/variables.tf
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-variable "team_name" {
-  description = "The name of your development team"
-  default     = "webops"
-}
-=======
->>>>>>> acd3184f5c42b7c28fcbaaed4be1b26dd6b15ce0
-
 variable "application" {
   description = "Name of Application you are deploying"
   default     = "example-app"
-}
-
-<<<<<<< HEAD
-variable "business_unit" {
-  description = "Area of the MOJ responsible for the service."
-  default     = "Platforms"
 }
 
 variable "is_production" {
@@ -22,21 +8,12 @@ variable "is_production" {
 }
 
 variable "namespace" {
-  default = "tom-namespace-dev"
-=======
-variable "namespace" {
   default = "example-team"
-}
-
-variable "business_unit" {
-  description = "Area of the MOJ responsible for the service."
-  default     = "Example"
 }
 
 variable "team_name" {
   description = "The name of your development team"
   default     = "example"
->>>>>>> acd3184f5c42b7c28fcbaaed4be1b26dd6b15ce0
 }
 
 variable "environment" {
@@ -44,7 +21,6 @@ variable "environment" {
   default     = "development"
 }
 
-<<<<<<< HEAD
 variable "business_unit" {
   description = "Area of the MOJ responsible for the service."
   default     = "Platforms"
@@ -56,21 +32,13 @@ variable "eks_cluster_name" {
   default     = "example_name"
 }
 
-
 variable "service_account_name" {
   description = "The name of the service account."
   type        = string
   default     = "example_name"
 }
 
-
-=======
 variable "infrastructure_support" {
   description = "The team responsible for managing the infrastructure. Should be of the form team-email."
   default     = "example@digital.justice.gov.uk"
 }
-
-variable "is_production" {
-  default = "false"
-}
->>>>>>> acd3184f5c42b7c28fcbaaed4be1b26dd6b15ce0
