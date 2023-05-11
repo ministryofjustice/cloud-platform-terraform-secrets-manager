2d9516c859de4581adacb04c0b25961c69ff78a8
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2023-05-11 12:10:32 +0100

    Fixing var references

--- main.tf
+++ main.tf
@@ -9,25 +9,25 @@ provider "aws" {
 resource "aws_secretsmanager_secret" "secret" {
   for_each                = { for k, v in var.secrets : k => v }
   description             = each.value.description != "" ? each.value.description : "Secret for ${each.value.name}"
-  name                    = "${each.value.namespace}/${each.value.name}"
+  name                    = "${var.namespace}/${each.value.name}"
   recovery_window_in_days = each.value.recovery-window-in-days # Set to 0 for no protection, between 7-30 days protection, default is 30.
 
   tags = {
-    team-name              = each.value.team-name
-    business-unit          = each.value.business-unit
-    application            = each.value.application
-    is-production          = each.value.is-production
-    namespace              = each.value.namespace
-    environment-name       = each.value.environment
-    infrastructure-support = each.value.infrastructure-support
+    team-name              = var.team_name
+    business-unit          = var.business_unit
+    application            = var.application
+    is-production          = var.is_production
+    namespace              = var.namespace
+    environment-name       = var.environment
+    infrastructure-support = var.infrastructure_support
   }
 }
 
 resource "aws_iam_policy" "policy" {
   for_each    = { for k, v in var.secrets : k => v }
-  name        = "${each.value.namespace}-policy-for-${each.value.name}-secret"
+  name        = "${var.namespace}-policy-for-${each.value.name}-secret"
   path        = "/"
-  description = "This policy is created to access the ${each.value.name} secret for the ${each.value.namespace} namespace."
+  description = "This policy is created to access the ${each.value.name} secret for the ${var.namespace} namespace."
 
   policy = {
     "Version": "2012-10-17",
@@ -38,7 +38,7 @@ resource "aws_iam_policy" "policy" {
                 "secretsmanager:GetSecretValue"
             ],
             "Effect": "Allow",
-            "Resource": "arn:aws:secretsmanager:eu-west-2:${data.aws_caller_identity.current.account_id}:secret:${each.value.namespace}/${each.value.name}"
+            "Resource": "arn:aws:secretsmanager:eu-west-2:${data.aws_caller_identity.current.account_id}:secret:${var.namespace}/${each.value.name}"
         },
         {
             "Sid": "Stmt1682592135009",
