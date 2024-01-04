f056550a5dd8e0056cc463fc5e8325bc091b8d93
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2024-01-04 16:53:44 +0000

    Add comment to examples about naming convention validations (#22)

--- examples/multiple_secrets.tf
+++ examples/multiple_secrets.tf
@@ -11,15 +11,18 @@ module "secrets_manager_multiple_secrets" {
   eks_cluster_name       = var.eks_cluster_name
 
   secrets = {
-    "test-secret-01" = {
+
+// the key "test-secret-01" is used to create kubernetes resource and must only contain lowercase alphanumeric characters, dots and dashes    
+"test-secret-01" = {
       description             = "test secret 01",
       recovery_window_in_days = 0
-      k8s_secret_name         = "test-secret-01"
+      k8s_secret_name         = "test-secret-01" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
     },
+    // the key "test-secret-02" is used to create kubernetes resource and must only contain lowercase alphanumeric characters, dots and dashes
     "test-secret-02" = {
       description             = "test secret 02",
       recovery_window_in_days = 0
-      k8s_secret_name         = "test-secret-02"
+      k8s_secret_name         = "test-secret-02" # The name of the secret in k8s and must only contain lowercase alphanumeric characters, dots and dashes
     },
   }
 }
