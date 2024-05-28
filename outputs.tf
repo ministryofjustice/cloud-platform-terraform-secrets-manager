45e8358cddb2fe4021c333a54b5de1cb94967639
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-05-28 21:32:42 +0100

    update for opensearch alert

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,13 @@ output "irsa_policy_arn" {
   value = aws_iam_policy.irsa_policy.arn
 }
 
-output "secret_ids" {
-  description = "The IDs of the AWS Secrets Manager secrets."
-  value       = { for k, v in aws_secretsmanager_secret.secret : k => v.id }
+# output "secret_ids" {
+#   description = "The IDs of the AWS Secrets Manager secrets."
+#   value       = { for k, v in aws_secretsmanager_secret.secret : k => v.id }
+# }
+
+# Create output for OpenSearch Alert Module, assume only the one secret in this case
+output "secret_id" {
+  description = "The ID of the AWS Secrets Manager secret."
+  value       = values(aws_secretsmanager_secret.secret)[0].id
 }
