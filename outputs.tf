ea2a1adc7d0c052632e1de86558c3acdf9b867aa
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-05-28 17:48:40 +0100

    update for opensearch alert

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,7 @@ output "irsa_policy_arn" {
   value = aws_iam_policy.irsa_policy.arn
 }
 
-output "secret_id" {
-  value = aws_secretsmanager_secret.secret.id[0]
+output "secret_ids" {
+  description = "The IDs of the AWS Secrets Manager secrets."
+  value       = { for k, v in aws_secretsmanager_secret.secret : k => v.id }
 }
