3775e3982ba357dc7b3cec2a7ef8e09944440da3
Marcus Aspin <maspin@unilink.com>
2026-01-26 17:04:24 +0000

    Add secret_names to outputs
    So that we can reference the secrets in other Terraform

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "irsa_policy_arn" {
   value = aws_iam_policy.irsa_policy.arn
 }
+
+output "secret_names" {
+  value = { for key in keys(var.secrets) : key => aws_secretsmanager_secret.secret[key].name }
+}
